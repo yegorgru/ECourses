f8d1be41e2d1b416d60f597f7047679c7fd7312b
Єгор Грушевий <yegorgru@gmail.com>
2021-06-24 20:00:14 +0300

    MyShared_ptr declaration

--- SmartPtrs/MyShared_ptr.h
+++ SmartPtrs/MyShared_ptr.h
@@ -3,6 +3,35 @@
 
 class MyShared_ptr
 {
+	struct Control {
+		size_t counter;
+	};
 
+public:
+	//Constructors, assignment operators, destructor
+	MyShared_ptr() noexcept;
+	explicit MyShared_ptr(CObject* ptr);
+	MyShared_ptr(const MyShared_ptr& r) noexcept;
+	MyShared_ptr(MyShared_ptr&& r) noexcept;
+
+	MyShared_ptr& operator=(const MyShared_ptr& r) noexcept;
+	MyShared_ptr& operator=(MyShared_ptr&& r) noexcept;
+
+	~MyShared_ptr();
+
+	//Modifiers
+	void reset(CObject* ptr);
+	void swap(MyShared_ptr& r) noexcept;
+
+	//Observers
+	CObject* get() const noexcept;
+	CObject& operator*() const;
+	CObject* operator->() const noexcept;
+	long use_count() const noexcept;
+	bool unique() const noexcept;
+	explicit operator bool() const noexcept;
+private:
+	CObject* m_Data;
+	Control* m_Control;
 };
 
