845649f1381259b18230c34d0cd0b97621e6676d
Єгор Грушевий <yegorgru@gmail.com>
2021-06-24 13:29:26 +0300

    ctor definitions

--- SmartPtrs/MyUnique_ptr.cpp
+++ SmartPtrs/MyUnique_ptr.cpp
@@ -1,13 +1,14 @@
 #include "MyUnique_ptr.h"
 
-MyUnique_ptr::MyUnique_ptr() noexcept
-{
-}
+#include <algorithm>
 
-MyUnique_ptr::MyUnique_ptr(CObject* p) noexcept
-{
-}
+MyUnique_ptr::MyUnique_ptr() noexcept :
+	m_Data(nullptr) {}
 
-MyUnique_ptr::MyUnique_ptr(MyUnique_ptr&& u) noexcept
-{
+MyUnique_ptr::MyUnique_ptr(CObject* p) noexcept :
+	m_Data(p) {}
+
+MyUnique_ptr::MyUnique_ptr(MyUnique_ptr&& u) noexcept {
+	std::swap(this->m_Data, u.m_Data);
+	u.m_Data = nullptr;
 }
