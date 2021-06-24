d443f68f9744d09d06df2c546ec67d4b0785a9e5
Єгор Грушевий <yegorgru@gmail.com>
2021-06-24 13:26:11 +0300

    ctor declarations and empty definitions

---
+++ SmartPtrs/MyUnique_ptr.cpp
@@ -0,0 +1,13 @@
+#include "MyUnique_ptr.h"
+
+MyUnique_ptr::MyUnique_ptr() noexcept
+{
+}
+
+MyUnique_ptr::MyUnique_ptr(CObject* p) noexcept
+{
+}
+
+MyUnique_ptr::MyUnique_ptr(MyUnique_ptr&& u) noexcept
+{
+}
--- SmartPtrs/MyUnique_ptr.h
+++ SmartPtrs/MyUnique_ptr.h
@@ -3,7 +3,9 @@
 
 class MyUnique_ptr {
 public:
-
+	MyUnique_ptr() noexcept;
+	explicit MyUnique_ptr(CObject* p) noexcept;
+	MyUnique_ptr(MyUnique_ptr&& u) noexcept;
 private:
 	CObject* m_Data;
 };
