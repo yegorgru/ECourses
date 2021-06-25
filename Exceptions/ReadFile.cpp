9c5dacce7964b9f9a86486c3e8f36492ef034a18
Єгор Грушевий <yegorgru@gmail.com>
2021-06-25 17:21:03 +0300

    read file with error codes implemented

--- Exceptions/ReadFile.cpp
+++ Exceptions/ReadFile.cpp
@@ -1,9 +1,46 @@
 #include "ReadFile.h"
 
-void read_file_exceptions() {
+#include <fstream>
+#include <string>
 
+
+void read_file_exceptions() {
+	
 }
 
 ErrorCode read_file_error_codes() {
-
+	auto code = ErrorCode::SuccessfullyRead;
+	std::ifstream fin("task.txt", std::ios::ate);
+	if (!fin.is_open()) {
+		code = ErrorCode::FileDidntOpen;
+	}
+	else {
+		long size = fin.tellg();
+		if (size <= 0) {
+			code = ErrorCode::UnknownSize;
+		}
+		else {
+			std::string fileData;
+			fileData.reserve(size);
+			if (fileData.capacity() != size) {
+				code = ErrorCode::NotEnoughMemory;
+			}
+			else {
+				try
+				{
+					fileData = std::string((std::istreambuf_iterator<char>(fin)),
+						std::istreambuf_iterator<char>());
+				}
+				catch (const std::exception&)
+				{
+					code = ErrorCode::ReadFailed;
+				}
+			}
+		}
+		fin.close();
+		if (fin.is_open() && code == ErrorCode::SuccessfullyRead) {
+			code = ErrorCode::FileDidntClose;
+		}
+	}
+	return code;
 }
