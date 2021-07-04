771027aac6b390b8e2c297c400e0f95170fe4253
Єгор Грушевий <yegorgru@gmail.com>
2021-07-04 13:35:47 +0300

    handle complex expressions test (calculation)

--- Calculator/GoogleTestsCalculator/test.cpp
+++ Calculator/GoogleTestsCalculator/test.cpp
@@ -110,5 +110,32 @@ TEST(CalculationTest, HandleMultiplicationDivision) {
     tree.SetExpression("(-15)/(-3)");
     EXPECT_EQ(tree.Calculate().ToString(), "5");
 }
-//  EXPECT_EQ(1, 1);
-//  EXPECT_TRUE(true);
+
+TEST(CalculationTest, HandleComplexExpressions) {
+    ExpressionTree tree("(5-3)");
+    EXPECT_EQ(tree.Calculate().ToString(), "2");
+    tree.SetExpression("(5)-(-3)");
+    EXPECT_EQ(tree.Calculate().ToString(), "8");
+    tree.SetExpression("-3-4");
+    EXPECT_EQ(tree.Calculate().ToString(), "-7");
+    tree.SetExpression("(5*3)-(-3-4)");
+    EXPECT_EQ(tree.Calculate().ToString(), "22");
+    tree.SetExpression("-(5*3)-(-3-4)");
+    EXPECT_EQ(tree.Calculate().ToString(), "-8");
+    tree.SetExpression("-(-(5*3)-(-3-4))");
+    EXPECT_EQ(tree.Calculate().ToString(), "8");
+    tree.SetExpression("-(-(5*3)-(-3-4/2))");
+    EXPECT_EQ(tree.Calculate().ToString(), "10");
+    tree.SetExpression("-(-(5*3))-(-3-4/2)");
+    EXPECT_EQ(tree.Calculate().ToString(), "20");
+    tree.SetExpression("10*10-20*3+5+5-11*2");
+    EXPECT_EQ(tree.Calculate().ToString(), "28");
+    tree.SetExpression("10*10-20*3+5+5-11*2");
+    EXPECT_EQ(tree.Calculate().ToString(), "28");
+    tree.SetExpression("(1)+(2)+(3)");
+    EXPECT_EQ(tree.Calculate().ToString(), "6");
+    tree.SetExpression("42/(-2)");
+    EXPECT_EQ(tree.Calculate().ToString(), "-21");
+    tree.SetExpression("35-19*2-(5-6/2)");
+    EXPECT_EQ(tree.Calculate().ToString(), "-5");
+}
