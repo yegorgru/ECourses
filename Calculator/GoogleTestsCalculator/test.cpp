7772cbd3809f89c2900b84663bceb87d02774d5d
Єгор Грушевий <yegorgru@gmail.com>
2021-07-04 13:28:47 +0300

    handle pkus and minus test (calculation)

--- Calculator/GoogleTestsCalculator/test.cpp
+++ Calculator/GoogleTestsCalculator/test.cpp
@@ -76,5 +76,20 @@ TEST(ValidationTest, HandleComplexExpressions) {
     s = "(5)-(-3)";
     EXPECT_TRUE(ExpressionTree::CheckValidity(s.begin(), s.end()));
 }
+
+TEST(CalculationTest, HandlePlusMinus) {
+    ExpressionTree tree("5+5");
+    EXPECT_EQ(tree.Calculate().ToString(), "10");
+    tree.SetExpression("5-5");
+    EXPECT_EQ(tree.Calculate().ToString(), "0");
+    tree.SetExpression("-5+3");
+    EXPECT_EQ(tree.Calculate().ToString(), "-2");
+    tree.SetExpression("5-10");
+    EXPECT_EQ(tree.Calculate().ToString(), "-5");
+    tree.SetExpression("-5-7");
+    EXPECT_EQ(tree.Calculate().ToString(), "-12");
+    tree.SetExpression("111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111+111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
+    EXPECT_EQ(tree.Calculate().ToString(), "222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222");
+}
 //  EXPECT_EQ(1, 1);
 //  EXPECT_TRUE(true);
