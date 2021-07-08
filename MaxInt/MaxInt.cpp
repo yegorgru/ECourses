5d113e05e9f6e00595b809cdb69183a23ca2b56b
Єгор Грушевий <yegorgru@gmail.com>
2021-07-08 17:59:41 +0300

    task done

--- MaxInt/MaxInt.cpp
+++ MaxInt/MaxInt.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
 
+template <int N>
+constexpr int MaxInt() {
+    return MaxInt<N - 1>()*10 + 9;
+}
+
+template <>
+constexpr int MaxInt<1>() {
+    return 9;
+}
 
 int main()
 {
+    std::cout << MaxInt<5>() << std::endl;
     return 0;
 }
